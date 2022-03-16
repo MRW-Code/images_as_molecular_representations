4751760b3624fc282609960c15591d33de74e740
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-03-16 12:19:13 +0000

    Add gpu index argument

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import torch
 
 parser = argparse.ArgumentParser(usage='python main.py')
 
@@ -7,13 +9,10 @@ parser.add_argument('-i', '--input', action='store', dest='input',
 parser.add_argument('-d', '--dataset', action='store', dest='dataset',
                     default='None', choices=['None', 'solubility', 'cocrystal'])
 parser.add_argument('--no_augs', action='store_true', dest='no_augs', default=False)
-
-
-
-# parser.add_argument('-m', '--model', action='store', dest='model',
-#                     default='RF', choices=['RF', 'ResNet'])
-# parser.add_argument('--kfold', action='store_true', dest='kfold')
-# parser.add_argument('-u', '--user', action='store', dest='user',
-#                     default='matt', choices=['laura', 'matt'])
-
+parser.add_argument('--gpu_idx', action='store', dest='gpu_idx', default='0',
+                  choices=['0', '1', '2', '3', '4', '5'])
+parser.add_argument('--cpu', action='store_true', dest='cpu', default=False)
 args = parser.parse_args()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_idx
+device = torch.device('cuda' if torch.cuda.is_available() and not args.cpu else 'cpu')
