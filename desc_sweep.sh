e92e2b6aea2fae9e6a8eb3f9c1abe7ba985fb1b8
Matthew Wilkinson <mattwilkinson16@hotmail.co.uk>
2022-03-21 09:42:01 +0000

    metrics sheet update

--- desc_sweep.sh
+++ desc_sweep.sh
@@ -31,34 +31,34 @@
 #python3 main.py -d cocrystal -i spectrophore
 
 ## SOLUBILITY IN TRIPLICATE
-python3 main.py -d solubility -i mordred_descriptor
-python3 main.py -d solubility -i mordred_descriptor
-python3 main.py -d solubility -i mordred_descriptor
-
-python3 main.py -d solubility -i rdkit_descriptor
-python3 main.py -d solubility -i rdkit_descriptor
-python3 main.py -d solubility -i rdkit_descriptor
-
-
-python3 main.py -d solubility -i mol2vec
-python3 main.py -d solubility -i mol2vec
-python3 main.py -d solubility -i mol2vec
-
-
-python3 main.py -d solubility -i ecfp
-python3 main.py -d solubility -i ecfp
-python3 main.py -d solubility -i ecfp
+#python3 main.py -d solubility -i mordred_descriptor
+#python3 main.py -d solubility -i mordred_descriptor
+#python3 main.py -d solubility -i mordred_descriptor
+#
+#python3 main.py -d solubility -i rdkit_descriptor
+#python3 main.py -d solubility -i rdkit_descriptor
+#python3 main.py -d solubility -i rdkit_descriptor
+#
+#
+#python3 main.py -d solubility -i mol2vec
+#python3 main.py -d solubility -i mol2vec
+#python3 main.py -d solubility -i mol2vec
+#
+#
+#python3 main.py -d solubility -i ecfp
+#python3 main.py -d solubility -i ecfp
+#python3 main.py -d solubility -i ecfp
 
 #python3 main.py -d solubility -i pubchem_fp
 #python3 main.py -d solubility -i pubchem_fp
 #python3 main.py -d solubility -i pubchem_fp
 
-python3 main.py -d solubility -i maccs
-python3 main.py -d solubility -i maccs
-python3 main.py -d solubility -i maccs
-
-python3 main.py -d solubility -i spectrophore
-python3 main.py -d solubility -i spectrophore
-python3 main.py -d solubility -i spectrophore
+#python3 main.py -d solubility -i maccs
+#python3 main.py -d solubility -i maccs
+#python3 main.py -d solubility -i maccs
+#
+#python3 main.py -d solubility -i spectrophore
+#python3 main.py -d solubility -i spectrophore
+#python3 main.py -d solubility -i spectrophore
 
 
